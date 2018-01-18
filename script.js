ee8d7aa5da58f17670509eac6444d1090c1e825b
Karolina <karolina@Air-Karolina.lan>
2018-01-18 18:14:15 +0100

    Built random quoto machine.

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-var url = 'http://api.icndb.com/jokes/random';
-    paragraph = document.getElementById('joke');
-
-
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
-  getJoke();
-});
-
-function getJoke() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.addEventListener('load', function(){
-      var response = JSON.parse(xhr.response);
-      paragraph.innerHTML = response.value.joke;
+var prefix = "https://cors-anywhere.herokuapp.com/";
+     tweetLink = "https://twitter.com/intent/tweet?text=";
+     quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+function getQuote() {
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
+}
+function createTweet(input) {
+    var data = input[0];
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
+    
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+    var tweetText = "Quote of the day - " + quoteText + "Author: " + quoteAuthor;
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author:" + quoteAuthor);
+        $('.tweet').attr('href', tweet);
+    }
+}
+$(document).ready(function() {
+        getQuote();
+        $('.trigger').click(function() {
+            getQuote();
+        });
     });
-    xhr.send();
-  }
-  console.log(getJoke());
+getQuote();
